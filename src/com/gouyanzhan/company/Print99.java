6ff72504f23bdfab71804da1dedb2f614a1dd968
gouyanzhan <gouyanzhan@yit.com>
2019-01-08 23:51:59 +0800

    1、把daoxu命名规范化成Daoxu； 2、测试任意a*b乘法表，并根据测试结果完善代码； 3、四种方法分别穷举出参数和返回值的组合类型；

--- src/com/gouyanzhan/company/Print99.java
+++ src/com/gouyanzhan/company/Print99.java
@@ -16,12 +16,16 @@ public class Print99 {
      * @param b int型数值2
      */
     public static void printMultiplyTable(int a, int b) {
-        System.out.print("                       " + a + " * " + b + " 乘 法 表 \n");
         if (b > a) {
             int temp = a;
             a = b;
             b = temp;
         }
+        if (a <= 0 || b <= 0) {
+            System.out.println("不能输入非正数");
+            return;
+        }
+        System.out.print("                       " + a + " * " + b + " 乘 法 表 \n");
         for (int i = 1; i <= a; i++) {
             for (int j = 1; j <= b; j++) {
                 if (i >= j) {
@@ -36,9 +40,20 @@ public class Print99 {
     }
 
     @Test
-    public void testPrintMultiplyTable() {
+    public void testPrintMultiplyTable1() {
         printMultiplyTable(8, 9);
     }
 
+    @Test
+    public void testPrintMultiplyTable2() {
+        printMultiplyTable(0, 9);
+    }
+
+    @Test
+    public void testPrintMultiplyTable3() {
+        printMultiplyTable(-1, 9);
+    }
+
 }
+
 
