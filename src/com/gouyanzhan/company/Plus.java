6f876174d5c137188e0a7aa7569f21482bf6ffe9
gouyanzhan <gouyanzhan@yit.com>
2019-01-10 23:53:30 +0800

    又写了一次单元测试！！！

---
+++ src/com/gouyanzhan/company/Plus.java
@@ -0,0 +1,24 @@
+package com.gouyanzhan.company;
+
+/**
+ * @author Yanzhan Gou
+ * @date 2019-01-10 23:28:23
+ */
+public class Plus {
+    public static void main(String[] args){
+    plus();
+    }
+
+    private static void plus(){
+        int a = 5;
+        System.out.println("a:"+a);
+        System.out.println();
+        int b = a++;
+        System.out.println("b:"+b);
+        System.out.println("a:"+a);
+        System.out.println();
+        int c = ++a;
+        System.out.println("c:"+c);
+        System.out.println("a:"+a);
+    }
+}
--- src/com/gouyanzhan/exercise/LuoJi2.java
+++ src/com/gouyanzhan/exercise/LuoJi2.java
@@ -1,10 +1,24 @@
 package com.gouyanzhan.exercise;
 
+import org.junit.Test;
+
 public class LuoJi2 {
-    public static void main(String[]args){
+    public static void main(String[] args) {
+        //int a = 5;
+      //  boolean b = (a < 4) && (a++ < 10);
+        printLojic(5);
+    }
+
+    private static void printLojic(int a) {
+        boolean boolVal;
+        boolVal = (a < 4) && (a++ < 10);
+        System.out.println("使用短路逻辑运算符的结果为" + boolVal);
+        System.out.println("a的结果为" + a);
+    }
+
+    @Test
+    public void testPrintLojic(){
         int a = 5;
-        boolean b = (a<4)&&(a++<10);
-        System.out.println("使用短路逻辑运算符的结果为" + b);
-        System.out.println("a的结果为" +a );
+       printLojic(a);
     }
 }
