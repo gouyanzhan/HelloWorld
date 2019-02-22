16fafae77d8134610a2885997b6b65d77b74b00c
siwuxie095 <lijiajing@yit.com>
2019-02-22 15:28:36 +0800

    测试一个Stream中使用多个filter的情况；

---
+++ src/com/siwuxie095/forme/test/test007/Main.java
@@ -0,0 +1,19 @@
+package com.siwuxie095.forme.test.test007;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Jiajing Li
+ * @date 2019-02-22 15:24:33
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        list = list.stream().filter(val -> val > 3).filter(val -> val < 8).collect(Collectors.toList());
+        System.out.println(list);
+    }
+
+}
