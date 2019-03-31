7eb4078bf725b40d46e9e91fec1b24f4bf97e57f
siwuxie095 <lijiajing@yit.com>
2019-03-31 14:59:28 +0800

    统计和生成所有不同的二叉树；

---
+++ src/com/siwuxie095/forme/algorithm/chapter3rd/question23th/answer1st/Node.java
@@ -0,0 +1,18 @@
+package com.siwuxie095.forme.algorithm.chapter3rd.question23th.answer1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2019-03-31 14:22:41
+ */
+public class Node {
+
+    public int value;
+
+    public Node left;
+
+    public Node right;
+
+    public Node(int data) {
+        this.value = data;
+    }
+}
