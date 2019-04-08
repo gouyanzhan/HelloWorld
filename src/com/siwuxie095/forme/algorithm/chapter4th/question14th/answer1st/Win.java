1c2b3ae6ffd3d89134bc6f2f564f236fd3a9fb17
siwuxie095 <lijiajing@yit.com>
2019-04-08 13:38:02 +0800

    排成一条线的纸牌博弈问题；

---
+++ src/com/siwuxie095/forme/algorithm/chapter4th/question14th/answer1st/Win.java
@@ -0,0 +1,46 @@
+package com.siwuxie095.forme.algorithm.chapter4th.question14th.answer1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2019-04-08 12:25:34
+ */
+public class Win {
+
+    public static int win1(int[] arr) {
+        if (null == arr || arr.length == 0) {
+            return 0;
+        }
+        return Math.max(f(arr, 0, arr.length - 1), s(arr, 0, arr.length - 1));
+    }
+
+    private static int f(int[] arr, int i, int j) {
+        if (i == j) {
+            return arr[i];
+        }
+        return Math.max(arr[i] + s(arr, i + 1, j), arr[j] + s(arr, i, j - 1));
+    }
+
+    private static int s(int[] arr, int i, int j) {
+        if (i == j) {
+            return 0;
+        }
+        return Math.min(f(arr, i + 1, j), f(arr, i, j - 1));
+    }
+
+
+    public static int win2(int[] arr) {
+        if (null == arr || arr.length == 0) {
+            return 0;
+        }
+        int[][] f = new int[arr.length][arr.length];
+        int[][] s = new int[arr.length][arr.length];
+        for (int j = 0; j < arr.length; ++j) {
+            f[j][j] = arr[j];
+            for (int i = j - 1; i >= 0; --i) {
+                f[i][j] = Math.max(arr[i] + s[i + 1][j], arr[j] + s[i][j - 1]);
+                s[i][j] = Math.min(f[i + 1][j], f[i][j - 1]);
+            }
+        }
+        return Math.max(f[0][arr.length - 1], s[0][arr.length - 1]);
+    }
+}
