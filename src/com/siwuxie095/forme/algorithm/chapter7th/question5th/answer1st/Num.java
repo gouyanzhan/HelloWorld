5fe3567193715895bed76ebdd6cf0ab22477d690
siwuxie095 <lijiajing@yit.com>
2019-05-19 14:44:10 +0800

    在其他数都出现偶数次的数组中找到出现奇数次的数；

---
+++ src/com/siwuxie095/forme/algorithm/chapter7th/question5th/answer1st/Num.java
@@ -0,0 +1,33 @@
+package com.siwuxie095.forme.algorithm.chapter7th.question5th.answer1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2019-05-19 14:10:21
+ */
+public class Num {
+
+    public static void printOddTimesNum1(int[] arr) {
+        int e0 = 0;
+        for (int curr : arr) {
+            e0 ^= curr;
+        }
+        System.out.println(e0);
+    }
+
+
+    public static void printOddTimesNum2(int[] arr) {
+        int eO = 0;
+        int eOhasOne = 0;
+        for (int curr : arr) {
+            eO ^= curr;
+        }
+        int rightOne = eO & (~eO + 1);
+        for (int curr : arr) {
+            if ((curr & rightOne) != 0) {
+                eOhasOne ^= curr;
+            }
+        }
+        System.out.println(eOhasOne + " " + (eO ^ eOhasOne));
+    }
+
+}
