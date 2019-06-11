d164eafaba119ed0c88f1a42369ebbc995f853ef
siwuxie095 <lijiajing@yit.com>
2019-06-11 23:08:40 +0800

    数组中未出现的最小正整数；

---
+++ src/com/siwuxie095/forme/algorithm/chapter8th/question25th/answer1st/Number.java
@@ -0,0 +1,30 @@
+package com.siwuxie095.forme.algorithm.chapter8th.question25th.answer1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2019-06-11 22:36:50
+ */
+public class Number {
+
+    public static int missNum(int[] arr) {
+        int l = 0;
+        int r = arr.length;
+        while (l < r) {
+            if (arr[l] == l + 1) {
+                l++;
+            } else if (arr[l] <= l || arr[l] > r || arr[arr[l] - 1] == arr[l]) {
+                arr[l] = arr[--r];
+            } else {
+                swap(arr, l, arr[l] - 1);
+            }
+        }
+        return l + 1;
+    }
+
+    private static void swap(int[] arr, int index1, int index2) {
+        int tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
+    }
+
+}
