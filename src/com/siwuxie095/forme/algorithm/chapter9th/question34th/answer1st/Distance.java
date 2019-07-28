9e2ed4c43e05ecc000e9040eeb9d72fbd47cb990
siwuxie095 <lijiajing@yit.com>
2019-07-29 00:24:09 +0800

    邮局选址问题；

---
+++ src/com/siwuxie095/forme/algorithm/chapter9th/question34th/answer1st/Distance.java
@@ -0,0 +1,70 @@
+package com.siwuxie095.forme.algorithm.chapter9th.question34th.answer1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2019-07-28 22:24:50
+ */
+public class Distance {
+
+    public static int minDistances1(int[] arr, int num) {
+        if (null == arr || num < 1 || arr.length < num) {
+            return 0;
+        }
+        int[][] w = new int[arr.length + 1][arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                w[i][j] = w[i][j - 1] + arr[j] - arr[(i + j) / 2];
+            }
+        }
+        int[][] dp = new int[num][arr.length];
+        for (int j = 0; j != arr.length; j++) {
+            dp[0][j] = w[0][j];
+        }
+        for (int i = 1; i < num; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = 0; k <= j; k++) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][k] + w[k + 1][j]);
+                }
+            }
+        }
+        return dp[num - 1][arr.length - 1];
+    }
+
+    public static int minDistances2(int[] arr, int num) {
+        if (null == arr || num < 1 || arr.length < num) {
+            return 0;
+        }
+        int[][] w = new int[arr.length + 1][arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                w[i][j] = w[i][j - 1] + arr[j] - arr[(i + j) / 2];
+            }
+        }
+        int[][] dp = new int[num][arr.length];
+        int[][] s = new int[num][arr.length];
+        for (int j = 0; j != arr.length; j++) {
+            dp[0][j] = w[0][j];
+            s[0][j] = 0;
+        }
+        int minK = 0;
+        int maxK = 0;
+        int cur = 0;
+        for (int i = 1; i < num; i++) {
+            for (int j = arr.length - 1; j > i; j--) {
+                minK = s[i - 1][j];
+                maxK = j == arr.length - 1 ? arr.length - 1 : s[i][j + 1];
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = minK; k <= maxK; k++) {
+                    cur = dp[i - 1][k] + w[k + 1][j];
+                    if (cur < dp[i][j]) {
+                        dp[i][j] = cur;
+                        s[i][j] = k;
+                    }
+                }
+            }
+        }
+        return dp[num - 1][arr.length - 1];
+    }
+
+}
