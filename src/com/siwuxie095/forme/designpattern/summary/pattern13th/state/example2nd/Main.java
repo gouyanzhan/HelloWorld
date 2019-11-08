dbb119a6c02ec7103a8c82c575e9b524436715f7
siwuxie095 <834879583@qq.com>
2019-11-08 16:28:34 +0800

    状态模式；

---
+++ src/com/siwuxie095/forme/designpattern/summary/pattern13th/state/example2nd/Main.java
@@ -0,0 +1,16 @@
+package com.siwuxie095.forme.designpattern.summary.pattern13th.state.example2nd;
+
+/**
+ * @author Jiajing Li
+ * @date 2019-11-08 14:33:07
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Context context = new Context();
+        context.request();
+        context.request();
+        context.request();
+    }
+
+}
