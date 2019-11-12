1f1afb5a86f82b68f3d72519cc8f74db57155000
siwuxie095 <834879583@qq.com>
2019-11-12 12:11:19 +0800

    享元模式；

---
+++ src/com/siwuxie095/forme/designpattern/summary/pattern18th/flyweight/example2nd/Flyweight.java
@@ -0,0 +1,29 @@
+package com.siwuxie095.forme.designpattern.summary.pattern18th.flyweight.example2nd;
+
+/**
+ * 抽象享元类
+ *
+ * @author Jiajing Li
+ * @date 2019-11-11 18:19:34
+ */
+abstract class Flyweight {
+
+    /**
+     * 内部状态
+     */
+    private final String intrinsic;
+
+
+    Flyweight(String intrinsic) {
+        this.intrinsic = intrinsic;
+    }
+
+    /**
+     * 操作，传入外部状态作为标识
+     */
+    abstract void operation(String extrinsic);
+
+    String getIntrinsic() {
+        return intrinsic;
+    }
+}
