11d4d5d35fda205267e905534bf2477893629506
siwuxie095 <834879583@qq.com>
2020-03-18 13:26:50 +0800

    测试负数取余是对应正数取余的相反数；

---
+++ src/com/siwuxie095/forme/test/test022/Main.java
@@ -0,0 +1,24 @@
+package com.siwuxie095.forme.test.test022;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-03-18 13:22:58
+ */
+public class Main {
+
+    /**
+     * 说明的问题：
+     * 1、负数取余是对应正数取余的相反数。
+     */
+    public static void main(String[] args) {
+        System.out.println(1 % 12);
+        System.out.println(-1 % 12);
+        System.out.println();
+        System.out.println(11 % 12);
+        System.out.println(-11 % 12);
+        System.out.println();
+        System.out.println(15 % 12);
+        System.out.println(-15 % 12);
+    }
+
+}
