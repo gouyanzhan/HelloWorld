77f8714776980ad7888748d5040a83f6865d413b
siwuxie095 <834879583@qq.com>
2020-04-02 19:12:56 +0800

    测试从对象的JSON字符串中得到对应属性的值；

--- src/com/siwuxie095/forme/test/test026/Main.java
+++ src/com/siwuxie095/forme/test/test026/Main.java
@@ -8,14 +8,19 @@ import com.alibaba.fastjson.JSON;
  */
 public class Main {
 
+    /**
+     * 说明的问题：
+     * 1、测试从对象的 JSON 字符串中得到对应属性的值。
+     */
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         User user = new User();
         user.address = new Address();
-        user.address.city = "Shanghai";
-        user.address.province = "Shanghai";
+        user.address.city = "Xiangyang";
+        user.address.province = "Hubei";
         user.address.country = "China";
         Object o = user;
         String str = JSON.toJSONString(o);
+        //String str = JSON.toJSONString(user);
         if (str.contains("city")) {
             int i = str.indexOf("city");
             str = str.substring(i);
